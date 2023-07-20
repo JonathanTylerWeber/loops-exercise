c26d756b043daabe6d1c8cbacf5d6ee4315aeeb3
JonathanTylerWeber <jonathantweber@gmail.com>
2023-07-20 13:31:17 +0800

    finished

---
+++ app.js
@@ -0,0 +1,21 @@
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i)
+// }
+
+// let num = 1;
+// while (num<=10){
+//     console.log(num);
+//     num++;
+// }
+
+// for (let i = 0; i <= 20; i += 2) {
+//     console.log(i)
+// }
+
+// for (let i = 1; i <= 20; i += 2) {
+//     console.log(i)
+// }
+
+for (let i = 10; i >= 0; i -= 1) {
+    console.log(i)
+}
